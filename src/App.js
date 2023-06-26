ebc0758d4d2e034c0e4f123adfd9b06ff57f3d68
lalitTrainline <lalit.kumar@thetrainline.com>
2023-06-26 16:39:17 +0530

    Splitted Date component and added the css styling

--- src/App.js
+++ src/App.js
@@ -28,22 +28,22 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date1={expenses[0].date}
+        date={expenses[0].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date1={expenses[1].date}
+        date={expenses[1].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date1={expenses[2].date}
+        date={expenses[2].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date1={expenses[3].date}
+        date={expenses[3].date}
       ></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 /** Create your first component */
 function ExpenseItem(props) {
-    const day = props.date1.toLocaleString('en-US', {day: '2-digit'});
-    const month = props.date1.toLocaleString('en-US', {month: 'long'});
-    const year = props.date1.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>
-                <div>{day}</div>
-                <div>{month}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount} </div>
