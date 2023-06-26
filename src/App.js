e995da63c8648d5a6d1bc610ebd03202bc64febd
lalitTrainline <lalit.kumar@thetrainline.com>
2023-06-26 15:51:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date('Jul 12 2011'),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date('Jul 12 2011') },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date('Jul 12 2012'),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date('Jul 12 2013'),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date1={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date1={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date1={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date1={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,21 @@
 import './ExpenseItem.css'
 
 /** Create your first component */
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 6, 23);
-    const expenseDescription = 'Health Insurance';
-    const expensePrice = 120.80;
+function ExpenseItem(props) {
+    const day = props.date1.toLocaleString('en-US', {day: '2-digit'});
+    const month = props.date1.toLocaleString('en-US', {month: 'long'});
+    const year = props.date1.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{expenseDescription}</h2>
-                <div className='expense-item__price'>${expensePrice} </div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount} </div>
             </div>
         </div>
     );
