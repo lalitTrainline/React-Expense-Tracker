bae5f0a2472648e9951d96ae4efb91d3120b3086
lalitTrainline <lalit.kumar@thetrainline.com>
2023-07-03 16:03:37 +0530

    use of state in a react app

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
 import Card from '../UI/Card';
 import ExpenseDate from '../Expenses/ExpenseDate';
 import './ExpenseItem.css'
+import { useState } from 'react';
 
 /** Create your first component */
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React');
+
+    const btnClickEvent = () => {
+        setTitle('Title changed');
+        console.log(title);
+    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount} </div>
             </div>
+            <button onClick={btnClickEvent}>BUTTON</button>
         </Card>
     );
 }
